fc28ee39ce462d88678401ad799a9f044c3ddea7
Dang Hoa <danghoa77@gmail.com>
2025-12-05 00:08:41 +0700

    Initial

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+        plugins: [
+            [
+                "@tamagui/babel-plugin",
+                {
+                    config: "./tamagui.config.js",
+                },
+            ],
+
+            "react-native-reanimated/plugin",
+        ],
+    };
+};
---
+++ tamagui.config.js
@@ -0,0 +1,7 @@
+const { createTamagui } = require('tamagui')
+const { tokens, themes } = require('@tamagui/themes')
+
+module.exports = createTamagui({
+    tokens,
+    themes
+})
